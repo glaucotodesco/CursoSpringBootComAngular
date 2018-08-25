9064e2226d22074dcefbe86f7369d857a5b3cded
= <glauco.todesco@abutua.com>
2018-08-25 20:01:55 -0300

    Criacao do componente angular. Finalizacao do Tutorial parte 5.

---
+++ HelloSpringBootRest/bin/src/main/resources/script.sql
@@ -0,0 +1,5 @@
+Insert Into Produto Values (1,'Produto A',10.5);
+Insert Into Produto Values (2,'Produto B',20.5);
+Insert Into Produto Values (3,'Produto C',30.5);
+Insert Into Produto Values (4,'Produto D',40.5);
+Insert Into Produto Values (5,'Produto E',50.5);
